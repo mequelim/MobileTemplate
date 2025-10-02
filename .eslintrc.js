0d42bf1e9b6bfb3a85593c5cef7c4da5b0fecef8
Pedro Mequelim <pedromequelim@gmail.com>
2025-10-02 17:28:18 -0300

    Update ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "error",
     "@typescript-eslint/explicit-function-return-type": "error",
-    // "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/typedef": "error",
     "array-bracket-spacing": [ "error", "always" ],
